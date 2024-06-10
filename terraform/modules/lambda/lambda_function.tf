11d4b4430920e4f5f5bf7400bd44a530c9b8b9b9
Jan Ritter <jan.ritter@trustedshops.de>
2024-06-10 16:27:04 +0200

    fix: replace deprecated go1.x runtime with provided.al2023

--- terraform/modules/lambda/lambda_function.tf
+++ terraform/modules/lambda/lambda_function.tf
@@ -2,8 +2,8 @@
 resource "aws_lambda_function" "this" {
   function_name    = var.function_name
   description      = "Monitor if the SES dedicated IPs in this account are listed on a blocklist and notify if it is the case."
-  handler          = "monitor"
-  runtime          = "go1.x"
+  handler          = "bootstrap"
+  runtime          = "provided.al2023"
   memory_size      = 128
   timeout          = 600
   role             = aws_iam_role.this.arn
